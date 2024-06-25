a94f2af7e0947f33b7deeb157893565e60183d97
RcdsDw <rafael.candidodasilva.pro@gmail.com>
2024-06-25 15:50:34 +0200

    add mailer

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./contact.css";
 
+import emailjs from '@emailjs/browser';
+
 export default function Contact() {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs
+        .sendForm("service_weaobhq","template_wcajg4i", form.current, {
+          publicKey: '0mHWmmh55Iduvm1da',
+        })
+        .then(
+          () => {
+            console.log('SUCCESS!');
+            form.current.reset();
+          },
+          (error) => {
+            console.log('FAILED...', error.text);
+          },
+        );
+    };
     return (
         <div id="containerContact">
             <div className="screen">
@@ -20,23 +41,23 @@ export default function Contact() {
                     </div>
                     </div>
                     <div className="screen-body-item">
-                    <div className="app-form">
+                    <form className="app-form" ref={form} onSubmit={sendEmail}>
                         <div className="app-form-group">
-                        <input className="app-form-control" placeholder="PRENOM"/>
+                            <input className="app-form-control" placeholder="PRENOM" type="text" name="first_name"/>
                         </div>
                         <div className="app-form-group">
-                        <input className="app-form-control" placeholder="NOM"/>
+                            <input className="app-form-control" placeholder="NOM" type="text" name="last_name"/>
                         </div>
                         <div className="app-form-group">
-                        <input className="app-form-control" placeholder="EMAIL"/>
+                            <input className="app-form-control" placeholder="EMAIL" type="email" name="email"/>
                         </div>
                         <div className="app-form-group message">
-                        <input className="app-form-control" placeholder="MESSAGE"/>
+                            <textarea className="app-form-control" placeholder="MESSAGE" name="message" maxLength={500}/>
                         </div>
                         <div className="app-form-group buttons">
-                        <button className="app-form-button">Envoyer</button>
+                            <input className="app-form-button" type="submit" value="Envoyer"/>
                         </div>
-                    </div>
+                    </form>
                     </div>
                 </div>
             </div>
